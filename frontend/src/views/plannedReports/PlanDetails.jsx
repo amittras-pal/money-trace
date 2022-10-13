b9c5fc509628291b57bd4ab69bfcd8bcd0679114
amittras-pal <pal.amittras@gmail.com>
2022-10-13 20:30:37 +0530

    Done.

--- frontend/src/views/plannedReports/PlanDetails.jsx
+++ frontend/src/views/plannedReports/PlanDetails.jsx
@@ -31,75 +31,73 @@ export default function PlanDetails() {
   if (isLoading) return <CenteredLoader />;
 
   return (
-    <>
-      <Box
-        sx={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-        }}>
-        <Box sx={{ display: "flex", width: "100%" }}>
-          <Box sx={{ width: "100%" }}>
-            <Text size="lg" weight={500}>
-              {reportDetails?.data.response.report.name}
-            </Text>
-            <Text color={"dimmed"} size="sm">
-              {reportDetails?.data.response.report.description}
-            </Text>
-            <Divider
-              mt="xs"
-              mb={isMobile ? "xs" : "md"}
-              color="gray"
-              variant="dashed"
-            />
-          </Box>
+    <Box
+      sx={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}>
+      <Box sx={{ display: "flex", width: "100%" }}>
+        <Box sx={{ width: "100%" }}>
+          <Text size="lg" weight={500}>
+            {reportDetails?.data.response.report.name}
+          </Text>
+          <Text color={"dimmed"} size="sm">
+            {reportDetails?.data.response.report.description}
+          </Text>
+          <Divider
+            mt="xs"
+            mb={isMobile ? "xs" : "md"}
+            color="gray"
+            variant="dashed"
+          />
         </Box>
-        {reportDetails?.data?.response?.expenses?.length > 0 ? (
-          <>
-            {isMobile ? (
-              <>
-                <SegmentedControl
-                  size="sm"
-                  mb="xs"
-                  color="indigo"
-                  value={view}
-                  onChange={setView}
-                  data={[
-                    { label: "Summary", value: "summary" },
-                    { label: "List", value: "list" },
-                  ]}
-                />
-                <SimpleGrid cols={1} sx={{ flexGrow: 1 }}>
-                  {view === "summary" && (
-                    <SummaryView data={reportDetails?.data?.response} />
-                  )}
-                  {view === "list" && (
-                    <ListView data={reportDetails?.data?.response} />
-                  )}
-                </SimpleGrid>
-              </>
-            ) : (
-              <SimpleGrid cols={2} sx={{ flexGrow: 1 }}>
-                <SummaryView data={reportDetails?.data?.response} />
-                <ListView data={reportDetails?.data?.response} />
-              </SimpleGrid>
-            )}
-          </>
-        ) : (
-          <Group
-            position="center"
-            align="center"
-            sx={{ flexDirection: "column" }}
-            py={24}>
-            <Image src={emptyState} width={isMobile ? 240 : 480} />
-            <Text color="dimmed" align="center" size="sm">
-              No transactions have been added to this report yet. Add from the
-              dashboard.
-            </Text>
-          </Group>
-        )}
       </Box>
-    </>
+      {reportDetails?.data?.response?.expenses?.length > 0 ? (
+        <>
+          {isMobile ? (
+            <>
+              <SegmentedControl
+                size="sm"
+                mb="xs"
+                color="indigo"
+                value={view}
+                onChange={setView}
+                data={[
+                  { label: "Summary", value: "summary" },
+                  { label: "List", value: "list" },
+                ]}
+              />
+              <SimpleGrid cols={1} sx={{ flexGrow: 1 }}>
+                {view === "summary" && (
+                  <SummaryView data={reportDetails?.data?.response} />
+                )}
+                {view === "list" && (
+                  <ListView data={reportDetails?.data?.response} />
+                )}
+              </SimpleGrid>
+            </>
+          ) : (
+            <SimpleGrid cols={2} sx={{ flexGrow: 1 }}>
+              <SummaryView data={reportDetails?.data?.response} />
+              <ListView data={reportDetails?.data?.response} />
+            </SimpleGrid>
+          )}
+        </>
+      ) : (
+        <Group
+          position="center"
+          align="center"
+          sx={{ flexDirection: "column" }}
+          py={24}>
+          <Image src={emptyState} width={isMobile ? 240 : 480} />
+          <Text color="dimmed" align="center" size="sm">
+            No transactions have been added to this report yet. Add from the
+            dashboard.
+          </Text>
+        </Group>
+      )}
+    </Box>
   );
 }
