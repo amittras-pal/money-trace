df29fa464ec5be8a26d0e027589f2848040c8657
amittras-pal <pal.amittras@gmail.com>
2023-08-07 16:12:31 +0530

    Dashboard || In-place sum by category.

--- frontend/src/components/BudgetItem.jsx
+++ frontend/src/components/BudgetItem.jsx
@@ -1,12 +1,26 @@
-import { Badge, Box, Group, Text, ThemeIcon, Tooltip } from "@mantine/core";
+import {
+  Badge,
+  Box,
+  Checkbox,
+  Group,
+  Text,
+  ThemeIcon,
+  Tooltip,
+} from "@mantine/core";
 import React, { useMemo } from "react";
 import { Icons } from "../constants/categories";
 import { useStyles } from "../modules/home/styles";
 import { formatCurrency } from "../utils";
 
-function BudgetItem({ category, subCategories, total }) {
+function BudgetItem({
+  category,
+  subCategories,
+  total,
+  showSelection,
+  selection,
+  onSelectionChange,
+}) {
   const { classes } = useStyles();
-
   const subItems = useMemo(() => {
     return subCategories.map((item) => ({
       ...item,
@@ -16,27 +30,37 @@ function BudgetItem({ category, subCategories, total }) {
 
   return (
     <Box className={classes.item}>
-      <Group position="apart" align="center" mb={2}>
-        <Badge color={subCategories[0].color} size="sm" radius="sm">
-          {category}
-        </Badge>
-        <Text fw="bold">{formatCurrency(total)}</Text>
-      </Group>
-      <Group spacing={6}>
-        {subItems.map(({ label, value, color, Icon }) => (
-          <Tooltip
-            label={`${label}: ${formatCurrency(value)}`}
-            withArrow
-            key={label}
-            events={{ hover: true, touch: true }}
-            position="right"
-          >
-            <ThemeIcon size="sm" variant="light" color={color}>
-              <Icon size={16} />
-            </ThemeIcon>
-          </Tooltip>
-        ))}
-      </Group>
+      {showSelection && (
+        <Checkbox
+          size="sm"
+          value={category}
+          checked={selection.includes(category)}
+          onChange={onSelectionChange}
+        />
+      )}
+      <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+        <Group align="center" mb={2} position="apart">
+          <Badge color={subCategories[0].color} size="sm" radius="sm">
+            {category}
+          </Badge>
+          <Text fw="bold">{formatCurrency(total)}</Text>
+        </Group>
+        <Group spacing={6}>
+          {subItems.map(({ label, value, color, Icon }) => (
+            <Tooltip
+              label={`${label}: ${formatCurrency(value)}`}
+              withArrow
+              key={label}
+              events={{ hover: true, touch: true }}
+              position="right"
+            >
+              <ThemeIcon size="sm" variant="light" color={color}>
+                <Icon size={16} />
+              </ThemeIcon>
+            </Tooltip>
+          ))}
+        </Group>
+      </Box>
     </Box>
   );
 }
