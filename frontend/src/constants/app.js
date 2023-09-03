cfe3d7dbbb13e80511a75561b7c035b978eeef36
amittras-pal <pal.amittras@gmail.com>
2023-09-03 15:40:43 +0530

    Location aware Shortcuts List Modal.

--- frontend/src/constants/app.js
+++ frontend/src/constants/app.js
@@ -1,4 +1,5 @@
 export const APP_TITLE = "MTrace";
 export const primaryColor = "indigo";
+export const planDetailsPath = /\/plans\/\w+/;
 export const urlMatcher =
   /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
