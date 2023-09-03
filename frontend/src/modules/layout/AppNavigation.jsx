822ae16bc6b5140ab11b78436be89206c457de90
amittras-pal <pal.amittras@gmail.com>
2023-09-03 17:44:21 +0530

    Update style privisioning in navigation

--- frontend/src/modules/layout/AppNavigation.jsx
+++ frontend/src/modules/layout/AppNavigation.jsx
@@ -5,6 +5,7 @@ import {
   Text,
   ThemeIcon,
   UnstyledButton,
+  createStyles,
 } from "@mantine/core";
 import { useHotkeys } from "@mantine/hooks";
 import { useMemo, useRef } from "react";
@@ -13,53 +14,6 @@ import { primaryColor } from "../../constants/app";
 import { ROUTES } from "../../constants/routes";
 import { useMediaMatch } from "../../hooks/useMediaMatch";
 import { useAppStyles } from "./styles";
-
-function NavLink({ onChange, ...route }) {
-  const { pathname } = useLocation();
-  const ref = useRef();
-  const isMobile = useMediaMatch();
-
-  const active = useMemo(() => {
-    return route.exactMatch
-      ? pathname === route.path
-      : pathname.includes(route.path);
-  }, [route, pathname]);
-
-  const navigateViaShortcut = () => {
-    if (!active && !isMobile) ref.current.click();
-  };
-  useHotkeys([[`shift+${route.shortcut}`, navigateViaShortcut]]);
-
-  return (
-    <UnstyledButton
-      ref={ref}
-      component={Link}
-      onClick={onChange}
-      to={route.path}
-      sx={(theme) => ({
-        display: "block",
-        width: "100%",
-        padding: theme.spacing.xs,
-        borderRadius: theme.radius.sm,
-        marginBottom: theme.spacing.sm,
-        color: theme.colors.dark[0],
-        backgroundColor: active ? theme.colors.dark[5] : "transparent",
-        boxShadow: active ? theme.shadows.md : "none",
-        "&:hover": {
-          backgroundColor: active ? theme.colors.dark[5] : theme.colors.dark[8],
-        },
-      })}
-    >
-      <Group>
-        <ThemeIcon color={primaryColor} variant={active ? "filled" : "light"}>
-          {route.icon}
-        </ThemeIcon>
-        <Text size="sm">{route.label}</Text>
-        {!isMobile && <Kbd ml="auto">Shift + {route.shortcut}</Kbd>}
-      </Group>
-    </UnstyledButton>
-  );
-}
 
 export default function AppNavigation(props) {
   const { classes } = useAppStyles();
@@ -79,5 +33,58 @@ export default function AppNavigation(props) {
         </>
       </Navbar.Section>
     </Navbar>
+  );
+}
+
+const useNavBtnStyle = createStyles((theme, { active }) => ({
+  navBtn: {
+    display: "block",
+    width: "100%",
+    padding: theme.spacing.xs,
+    borderRadius: theme.radius.sm,
+    marginBottom: theme.spacing.sm,
+    color: theme.colors.dark[0],
+    backgroundColor: active ? theme.colors.dark[5] : "transparent",
+    boxShadow: active ? theme.shadows.md : "none",
+    "&:hover": {
+      backgroundColor: active ? theme.colors.dark[5] : theme.colors.dark[8],
+    },
+  },
+}));
+
+function NavLink({ onChange, ...route }) {
+  const { pathname } = useLocation();
+  const ref = useRef();
+  const isMobile = useMediaMatch();
+
+  const active = useMemo(() => {
+    return route.exactMatch
+      ? pathname === route.path
+      : pathname.includes(route.path);
+  }, [route, pathname]);
+
+  const { classes } = useNavBtnStyle({ active });
+
+  const navigateViaShortcut = () => {
+    if (!active && !isMobile) ref.current.click();
+  };
+  useHotkeys([[route.shortcut, navigateViaShortcut]]);
+
+  return (
+    <UnstyledButton
+      ref={ref}
+      component={Link}
+      onClick={onChange}
+      to={route.path}
+      className={classes.navBtn}
+    >
+      <Group>
+        <ThemeIcon color={primaryColor} variant={active ? "filled" : "light"}>
+          {route.icon}
+        </ThemeIcon>
+        <Text size="sm">{route.label}</Text>
+        {!isMobile && <Kbd ml="auto">{route.shortcut}</Kbd>}
+      </Group>
+    </UnstyledButton>
   );
 }
