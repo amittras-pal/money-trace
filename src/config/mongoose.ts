ca87edfbd14bd0aa6554e7b946d88339e7cd1949
Amittras Pal <72859660+amittras-pal@users.noreply.github.com>
2023-11-16 09:54:38 +0000

    Seed categories for a fresh DB. [TEST Pending]

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,4 +1,16 @@
 import mongoose from "mongoose";
+import data from "../data/categories.json";
+import Category from "../models/category.model";
+
+// TODO: move into utilities if this works properly...
+// Seed the category data inot the db if not vailable.
+const seedCategories = async () => {
+  const categories = await Category.count({});
+  if (categories === 0) {
+    const seedData = [...data.categories];
+    Category.insertMany(seedData);
+  }
+};
 
 // From Mongoose official docs: https://mongoosejs.com/docs/lambda.html
 let conn: any = null;
@@ -9,7 +21,10 @@ export const connectMongo = async (URI: string) => {
       .connect(URI, {
         serverSelectionTimeoutMS: 5000,
       })
-      .then(() => mongoose)
+      .then(() => {
+        seedCategories();
+        return mongoose;
+      })
       .catch((e) => console.error(e));
 
     // awaiting connection after assigning to the "conn" variable
