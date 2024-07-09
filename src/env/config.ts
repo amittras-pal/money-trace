49bc3ff632fe1bec2ffdaaf13bfc470a23276e65
amittras-pal <pal.amittras@gmail.com>
2024-07-09 09:19:49 +0530

    Environment Config for Changelog API.

--- src/env/config.ts
+++ src/env/config.ts
@@ -9,6 +9,9 @@ interface IEnv {
   JWT_SECRET?: string;
   NODE_ENV?: string;
   ORIGINS?: (string | RegExp)[];
+  OCTO_PK?: string;
+  OCTO_APP_ID: number;
+  OCTO_INST_ID: number;
 }
 
 export function getEnv(): IEnv {
@@ -18,6 +21,9 @@ export function getEnv(): IEnv {
     DB_URI: process.env.DB_URI ?? "",
     JWT_SECRET: process.env.JWT_SECRET ?? "",
     NODE_ENV: process.env.NODE_ENV,
+    OCTO_PK: process.env.OCTO_PK,
+    OCTO_APP_ID: parseInt(process.env.OCTO_APP_ID ?? ""),
+    OCTO_INST_ID: parseInt(process.env.OCTO_INST_ID ?? ""),
     ORIGINS: transformOrigins(process.env.ORIGINS?.split(",")),
   };
 }
