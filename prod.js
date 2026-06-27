d7e47b3dd962bce700b25203ed91c827bee38a4a
Amittras Pal <pal.amittras@gmail.com>
2026-06-27 10:05:55 +0530

    fix port for prod

--- prod.js
+++ prod.js
@@ -2,6 +2,6 @@ const app = require("./build/index");
 const { connectMongo } = require("./build/config/mongoose");
 
 connectMongo(process.env.DB_URI);
-app.default.listen(process.env.PORT ?? 5000, () => {
+app.default.listen(process.env.PORT ?? 3000, () => {
   console.log("Server Started...");
 });
